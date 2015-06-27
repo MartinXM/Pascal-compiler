95e1ceefee66807c15b36f197e00c558da9ed947
LoserKe <kehycs@foxmail.com>
2015-06-28 05:38:57 +0800

    Add OpKind in TreeNode.

---
+++ src/tree/OpKind.java
@@ -0,0 +1,18 @@
+package tree;
+
+/**
+ * Created by kehanyang on 15/6/28.
+ */
+public enum OpKind {
+    PLUS,
+    MINUS,
+    MUL,
+    DIV,
+    MOD,
+    LT,
+    LE,
+    GT,
+    GE,
+    EQUAL,
+    UNEQUAL;
+}
